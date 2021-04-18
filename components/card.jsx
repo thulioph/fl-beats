2d322ccd1b3c158c88bc688990be2d80b89dd980
Thulio Philipe <thulioph@gmail.com>
2021-04-18 01:11:10 -0300

    feat: enable iframe lazy loading

--- components/card.jsx
+++ components/card.jsx
@@ -19,7 +19,7 @@ const Card = (props) => {
           height="300"
           scrolling="no"
           frameBorder="no"
-          allow="autoplay"
+          loading="lazy"
           src={`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${beat.trackId}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true`}>
         </iframe>
 
